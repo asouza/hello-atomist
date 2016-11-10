e119b0441bd3bcab2c9f2302cb5686c5919c10ac
Alberto Luiz Souza <alots.ssa@gmail.com>
2016-11-10 20:09:36 -0200

    Initial commit from the atomist bot

---
+++ src/main/java/br/com/ideiasaleatorias/helloatomist/BootApplication.java
@@ -0,0 +1,12 @@
+package br.com.ideiasaleatorias.helloatomist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootApplication.class, args);
+    }
+}
